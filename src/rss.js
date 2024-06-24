d114a0335106911bbcaa8b0b0f8e247d127d939f
Anton Sekachev <a9185080045@gmail.com>
2024-06-24 19:13:43 +0300

    refactoring

--- src/rss.js
+++ src/rss.js
@@ -1,34 +1,5 @@
 const axios = require('axios');
-
-const parseRss = (data) => {
-  const parser = new DOMParser();
-  const rss = parser.parseFromString(data, 'application/xml');
-  const parseErrorNode = rss.querySelector('parsererror');
-  if (parseErrorNode) {
-    const errorText = `Parse error: ${parseErrorNode.textContent}`;
-    throw new Error(errorText);
-  }
-
-  const items = rss.querySelectorAll('item');
-  const feedTitle = rss.querySelector('channel > title').textContent;
-  const feedDescription = rss.querySelector('channel > description').textContent;
-
-  const posts = [...items].reduce((acc, item) => {
-    const postTitle = item.querySelector('title').textContent;
-    const link = item.querySelector('link').textContent;
-    const postDescription = item.querySelector('description').textContent;
-    return [
-      ...acc,
-      {
-        title: postTitle,
-        description: postDescription,
-        link,
-      },
-    ];
-  }, []);
-
-  return { title: feedTitle, description: feedDescription, posts };
-};
+import parseRss from './parser';
 
 const addPostsId = (content) => {
   const { posts, ...rest } = content;
@@ -40,12 +11,15 @@ const addPostsId = (content) => {
   return { posts: postsWithId, ...rest };
 };
 
-const getContent = (url) => {
+const proxyRequest = (url) => {
   const allOriginsUrl = new URL('get', 'https://allorigins.hexlet.app');
   allOriginsUrl.searchParams.set('disableCache', true);
   allOriginsUrl.searchParams.set('url', url);
-  return axios
-    .get(allOriginsUrl)
+  return axios.get(allOriginsUrl);
+};
+
+const getContent = (url) => {
+  return proxyRequest(url)
     .then((response) => response.data)
     .then((data) => ({ url, ...addPostsId(parseRss(data.contents)) }))
     .catch((error) => {
